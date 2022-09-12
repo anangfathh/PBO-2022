5cbe084a7371cc4e66e2f1b2b21e4912583d49dc
anangfath <89348743+anangfathh@users.noreply.github.com>
2022-09-12 13:01:30 +0700

    Add files via upload

---
+++ Konstanta.java
@@ -0,0 +1,10 @@
+public class Konstanta {
+    public static void main(String[] args) {
+        // Deklarasikan konstanta pi di sini
+        final double pi = 3.14;
+        // Deklarasikan variabel radius di sini
+        double radius = 20.50;
+
+        System.out.println("Area: " + (pi * radius * radius));
+    }
+}
