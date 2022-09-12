91af6c509163a73a8181860a244c6086b228bfd7
anangfath <89348743+anangfathh@users.noreply.github.com>
2022-09-12 12:58:42 +0700

    Add files via upload

---
+++ Variabel.java
@@ -0,0 +1,14 @@
+public class Variabel {
+    public static void main(String[] args) {
+        // Deklarasikan variabel di sini, sesuai dengan tipe datanya
+        int var1 = 123;
+        double var2 = 243.584;
+        String var3 = "Hello";
+        
+
+        // Cetak variabel di sini
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+    }
+}
